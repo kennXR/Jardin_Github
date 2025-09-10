08214473742bdac99d26d2f7c99869fabaeeddf6
Kenn <xramos@centro.edu.mx>
2025-09-09 23:53:33 -0600

    Tarea01:Creación de líneas

--- 04-Martes-9-Sep/lineas.js
+++ 04-Martes-9-Sep/lineas.js
@@ -1,6 +1,24 @@
 const canvas = document.getElementById("lienzo");
+console.log(canvas);
 const ctx = canvas.getContext("2d");
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+const W = canvas.width;
+const H = canvas.height;
+
+
+ctx.fillStyle = "#ffffff";
+
+
+
+ctx.strokeStyle = "blue";
+ctx.lineWidth = 2;
+
+for (let y = 0; y < H; y += 20) {
+  ctx.beginPath();
+  ctx.moveTo(0, y);
+  ctx.lineTo(W, y);
+  ctx.stroke();
+}
