4656c9fb88cb95196a56d21e1f8a796233da95ed
Kenn <xramos@centro.edu.mx>
2025-09-11 23:18:50 -0600

    Creacion tarea6

---
+++ 06-Jueves-11-Sep/op.js
@@ -0,0 +1,9 @@
+const canvas = document.getElementById("lienzo");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const W = canvas.width;
+const H = canvas.height;
+
